b6944a69e34d1154a42523002d61d160ee33ce72
Tim Spencer <tspencer103@gmail.com>
2016-03-03 23:44:44 -0600

    initial commmit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wp_another_social_links_widget
+
+Displays social media icons and links in Wordpress sidebar.  Code can easily be modified to add or change icons.  Facebook, Twitter, LinkedIn and Google+ provided.
---
+++ includes/social-links-scripts.php
@@ -0,0 +1,11 @@
+<?php
+
+// Add scripts
+function sl_add_scripts() {
+
+	wp_enqueue_style('sl-main-style', plugins_url() . '/social-links/css/style.css');
+	wp_enqueue_style('sl-main-script', plugins_url() . '/social-links/js/main.js');
+
+}
+
+add_action('wp_enqueue_scripts', 'sl_add_scripts');
